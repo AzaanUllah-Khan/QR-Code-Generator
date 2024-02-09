6f4e8685daacfe9dbb2960b1dee7d29c1d6330c4
AzaanUllah-Khan <azaanullahkhan291@gmail.com>
2024-02-10 02:52:04 +0500

    destination changing and validation error animation added

--- script.js
+++ script.js
@@ -7,8 +7,6 @@ function genQR(format){
     qrdata = document.getElementById("input").value
     formatS = format
     const icons = document.querySelectorAll('i');
-    if(qrdata){
-    
     icons.forEach(icons => {
       icons.addEventListener('click', function() {
         const currentActive = document.querySelector('i.active');
@@ -17,9 +15,7 @@ function genQR(format){
         }
         this.classList.add('active');
       });
-    });
-
-    
+    });    
     buttons.forEach(buttons => {
       buttons.addEventListener('click', function() {
         const currentActive = document.querySelector('button.active');
@@ -29,6 +25,8 @@ function genQR(format){
         this.classList.add('active');
       });
     });
+
+    if(qrdata){
         document.getElementById("img").src = `https://api.qrserver.com/v1/create-qr-code/?size=130x130&color=${colorS}&format=${formatS}&bgcolor=${bgC}&data=${qrdata}`
     }
 }
@@ -37,10 +35,25 @@ function changeColor(color){
         colorS = color
         document.getElementById("img").src = `https://api.qrserver.com/v1/create-qr-code/?size=130x130&color=${colorS}&format=${formatS}&bgcolor=${bgC}&data=${qrdata}`
     }
+    else{
+        document.getElementById("input").style.backgroundColor = "#ff5f5f2b"
+        setTimeout(()=>{
+            document.getElementById("input").style.backgroundColor = "transparent"
+        },1000)
+    }
 }
 function changeBG(bg){
     if(qrdata){
         bgC = bg
         document.getElementById("img").src = `https://api.qrserver.com/v1/create-qr-code/?size=130x130&color=${colorS}&format=${formatS}&bgcolor=${bgC}&data=${qrdata}`
     }
+    else{
+        document.getElementById("input").style.backgroundColor = "#ff5f5f2b"
+        setTimeout(()=>{
+            document.getElementById("input").style.backgroundColor = "transparent"
+        },1000)
+    }
+}
+function changeD(what){
+    document.getElementById("destinition").innerText = what
 }
